817b6eacb7d1994945f26da9f35567d9810f119e
Daniel John McLaughlan <daniel-j-m@live.com>
2022-11-23 01:22:59 +0000

    Key Now Rotates and moves up and down

--- I.See.You/Assets/SecurityDoor/KeyScript.cs
+++ I.See.You/Assets/SecurityDoor/KeyScript.cs
@@ -6,16 +6,24 @@ public class KeyScript : MonoBehaviour
 {
     private GameObject UIElement;
     public bool Range = false;
+    public float RotateSpeed = 5f;
+    public float FloatHeight = 0.3f;
+    public float FloatSpeed = 3f;
+    private Vector3 StartPos;
     //public GameObject SecurityDoor;
     //private SecurityDoorScript Scripts;
 
     private void Start()
     {
         UIElement = GameObject.FindGameObjectWithTag("UIKeyCounter");
+        StartPos = transform.position;
     }
 
     private void Update()
     {
+        transform.Rotate(0, 6.0f * RotateSpeed * Time.deltaTime, 0);
+
+        transform.position = new Vector3(transform.position.x, StartPos.y + (Mathf.Sin(Time.time * FloatSpeed) * FloatHeight), transform.position.z);
 
         if (Range == true)
         {
